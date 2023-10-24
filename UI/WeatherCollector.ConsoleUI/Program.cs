058bc5be5f848546d8e0dd3832ccf942858059f2
Swift <swiftx37@gmail.com>
2023-10-24 13:16:48 +0200

    ConsoleUI project setup.

--- UI/WeatherCollector.ConsoleUI/Program.cs
+++ UI/WeatherCollector.ConsoleUI/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly.Extensions.Http;
+using Polly;
+
+class Program
+{
+    private static IHost? _Hosting;
+
+    public static IHost Hosting => _Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+    public static IServiceProvider Services => Hosting.Services;
+
+    public static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder()
+        .ConfigureServices(ConfigureServices);
+
+    private static void ConfigureServices(HostBuilderContext host, IServiceCollection services) { }
+
+    public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        var jitter = new Random();
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(10, retryAttempt =>
+                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) +
+                TimeSpan.FromMilliseconds(jitter.Next(0, 1000)));
+    }
+
+    public static async Task Main(string[] args)
+    {
+        using var host = Hosting;
+        await host.StartAsync();
+
+        Console.WriteLine("Completed!");
+        Console.ReadLine();
+
+        await host.StopAsync();
+    }
+}
