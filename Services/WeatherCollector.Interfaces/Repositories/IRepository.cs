24b46b4fe647a7613c85ad4ac6518b07ff18942b
Swift <swiftx37@gmail.com>
2023-10-24 15:07:06 +0200

    Repository interface.

---
+++ Services/WeatherCollector.Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using WeatherCollector.Interfaces.Entities;
+
+namespace WeatherCollector.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<bool> ExistbyId(int id, CancellationToken cancellation = default);
+
+        Task<bool> Exist(T entity, CancellationToken cancellation = default);
+
+        Task<int> GetCount(CancellationToken cancellation = default);
+
+        Task<IEnumerable<T>> GetAll(CancellationToken cancellation = default);
+
+        Task<IEnumerable<T>> GetAll(int skip, int count, CancellationToken cancellation = default);
+
+        Task<IPage<T>> GetPage(int index, int size, CancellationToken cancellation = default);
+
+        Task<T> Get(int id, CancellationToken cancellation = default);
+
+        Task<T> Add(T entity, CancellationToken cancellation = default);
+
+        Task<T> Update(T entity, CancellationToken cancellation = default);
+
+        Task<T> Delete(T entity, CancellationToken cancellation = default);
+
+        Task<T> DeleteById(int id, CancellationToken cancellation = default);
+    }
+
+    public interface IPage<out T>
+    {
+        IEnumerable<T> Items { get; }
+
+        int Index { get; }
+
+        int Size { get; }
+
+        int TotalItemsCount { get; }
+
+        int TotalPagesCount { get; }
+    }
+}
