58727f9e2eb676d1d966314abc98fb10ea9d72c0
Swift <swiftx37@gmail.com>
2023-10-25 14:37:23 +0200

    Web repository preperation.

---
+++ Services/WeatherCollector.Clients/Repositories/WebRepository.cs
@@ -0,0 +1,67 @@
+﻿using WeatherCollector.Interfaces.Entities;
+using WeatherCollector.Interfaces.Repositories;
+
+namespace WeatherCollector.Clients.Repositories
+{
+    public class WebRepository<T> : IRepository<T> where T : IEntity
+    {
+        private readonly HttpClient _client;
+
+        public WebRepository(HttpClient client) => _client = client;
+
+        public Task<T?> Add(T? entity, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> Delete(T? entity, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> DeleteById(int id, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exist(T? entity, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistById(int id, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> Get(int skip, int count, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> Get(int id, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll(CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCount(CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IPage<T>> GetPage(int index, int size, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> Update(T? entity, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
