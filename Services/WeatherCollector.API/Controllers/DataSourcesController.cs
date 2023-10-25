80eb702e95da4a94c016a1f730070989956fc2df
Swift <swiftx37@gmail.com>
2023-10-25 12:47:47 +0200

    Web API controller for working with database repository.

---
+++ Services/WeatherCollector.API/Controllers/DataSourcesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherCollector.DAL.Entities;
+using WeatherCollector.Interfaces.Repositories;
+
+namespace WeatherCollector.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSourcesController : Controller
+    {
+        private readonly IRepository<DataSource> _repository;
+
+        public DataSourcesController(IRepository<DataSource> repository) => _repository = repository;
+
+        [HttpGet("count")]
+        public async Task<IActionResult> GetItemsCount() => Ok(await _repository.GetCount());
+    }
+}
--- Services/WeatherCollector.API/Program.cs
+++ Services/WeatherCollector.API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
 builder.Services.AddScoped(typeof(INamedRepository<>), typeof(DbNamedRepository<>));
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -40,31 +41,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
