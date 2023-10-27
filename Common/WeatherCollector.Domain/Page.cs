7617d7c066f940a91275d9cd504f63e04e936705
Swift <swiftx37@gmail.com>
2023-10-27 17:34:35 +0200

    Creating the Page<T> class in the Domain project.

---
+++ Common/WeatherCollector.Domain/Page.cs
@@ -0,0 +1,17 @@
+﻿using WeatherCollector.Interfaces;
+
+namespace WeatherCollector.Domain
+{
+    public class Page<T> : IPage<T>
+    {
+        public IEnumerable<T> Items { get; init; } = Enumerable.Empty<T>();
+
+        public int Index { get; init; }
+
+        public int Size { get; init; }
+
+        public int TotalItemsCount { get; init; }
+
+        public int TotalPagesCount => (int)Math.Ceiling((double)TotalItemsCount / Size);
+    }
+}
