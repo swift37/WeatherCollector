079cc67ed9893a7f563406531788aa7f2d9738d3
Swift <swiftx37@gmail.com>
2023-11-05 01:48:47 +0100

    Adding the Response model for services.

---
+++ WeatherCollector.BlazorUI/Services/Base/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherCollector.BlazorUI.Services.Base
+{
+    public class Response<T>
+    {
+        public string FaultMessage { get; set; } = string.Empty;
+
+        public bool Success { get; set; } = true;
+
+        public T Data { get; set; }
+    }
+}
