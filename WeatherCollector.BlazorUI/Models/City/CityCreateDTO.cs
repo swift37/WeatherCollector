8aa0f6593360353c3f15111c82bf5a0b2e9de986
Swift <swiftx37@gmail.com>
2023-11-05 01:47:03 +0100

    Adding CityCreateDTO and CityUpdateDTO models.

---
+++ WeatherCollector.BlazorUI/Models/City/CityCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherCollector.BlazorUI.Models.City
+{
+    public class CityCreateDTO
+    {
+        [Required(ErrorMessage = "The city name is required.")]
+        public string? Name { get; set; }
+
+        public string? Country { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
---
+++ WeatherCollector.BlazorUI/Models/City/CityUpdateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherCollector.BlazorUI.Models.City
+{
+    public class CityUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The city name is required.")]
+        public string? Name { get; set; }
+
+        public string? Country { get; set; }
+
+        [Required(ErrorMessage = "The latitude is required.")]
+        public double Latitude { get; set; }
+
+        [Required(ErrorMessage = "The longitude is required.")]
+        public double Longitude { get; set; }
+    }
+}
