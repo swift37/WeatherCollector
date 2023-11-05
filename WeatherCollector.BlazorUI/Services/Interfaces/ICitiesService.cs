3de1b7610bb1589f532fbc3adec66de268e2a6d4
Swift <swiftx37@gmail.com>
2023-11-05 02:10:18 +0100

    Adding the CitiesService interface.

---
+++ WeatherCollector.BlazorUI/Services/Interfaces/ICitiesService.cs
@@ -0,0 +1,19 @@
+﻿using WeatherCollector.BlazorUI.Models.City;
+using WeatherCollector.BlazorUI.Services.Base;
+using WeatherCollector.Domain;
+
+namespace WeatherCollector.BlazorUI.Services.Interfaces
+{
+    public interface ICitiesService
+    {
+        Task<Response<IEnumerable<City>>> GetAll();
+
+        Task<Response<City>> Get(int id);
+
+        Task<Response<City>> Create(CityCreateDTO cityCreateDTO);
+
+        Task<Response<City>> Update(CityUpdateDTO cityUpdateDTO);
+
+        Task<Response<City>> Delete(City city);
+    }
+}
