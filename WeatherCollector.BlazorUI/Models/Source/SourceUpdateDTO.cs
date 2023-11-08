5906cf6102042f2bd00d0191a331c12a8aa12b80
Swift <swiftx37@gmail.com>
2023-11-08 20:01:35 +0100

    Creating the SourceCreateDTO and SourceUpdateDTO.

---
+++ WeatherCollector.BlazorUI/Models/Source/SourceUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherCollector.BlazorUI.Models.Source
+{
+    public class SourceUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The source name is required.")]
+        public string? Name { get; set; }
+
+        public string? Details { get; set; }
+    }
+}
