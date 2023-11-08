607c5e727e31e278bb7ebd19eb45930f6b54bd93
Swift <swiftx37@gmail.com>
2023-11-08 20:02:38 +0100

    Creating the SourcesService interface.

---
+++ WeatherCollector.BlazorUI/Services/Interfaces/ISourcesService.cs
@@ -0,0 +1,19 @@
+﻿using WeatherCollector.BlazorUI.Models.Source;
+using WeatherCollector.BlazorUI.Services.Base;
+using WeatherCollector.Domain;
+
+namespace WeatherCollector.BlazorUI.Services.Interfaces
+{
+    public interface ISourcesService
+    {
+        Task<Response<IEnumerable<Source>>> GetAll();
+
+        Task<Response<Source>> Get(int id);
+
+        Task<Response<Source>> Create(SourceCreateDTO sourceCreateDTO);
+
+        Task<Response<Source>> Update(SourceUpdateDTO sourceUpdateDTO);
+
+        Task<Response<Source>> Delete(Source source);
+    }
+}
