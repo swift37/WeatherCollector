b2e14d68b3fcc6f7d59c96b72e7252ecab6d83bd
Swift <swiftx37@gmail.com>
2023-11-08 20:02:03 +0100

    Creating the SourceMappingProfile.

---
+++ WeatherCollector.BlazorUI/Infrastructure/Mapping/SourceMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WeatherCollector.BlazorUI.Models.Source;
+using WeatherCollector.Domain;
+
+namespace WeatherCollector.BlazorUI.Infrastructure.Mapping
+{
+    public class SourceMappingProfile : Profile
+    {
+        public SourceMappingProfile()
+        {
+            CreateMap<SourceCreateDTO, Source>().ReverseMap();
+
+            CreateMap<SourceUpdateDTO, Source>().ReverseMap();
+        }
+    }
+}
